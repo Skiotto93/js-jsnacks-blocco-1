709839736bb482510df05f2a3c28bb7ddb47b142
skiotto93 <fabio.forenza@hotmail.com>
2022-10-26 12:36:24 +0200

    Aggiunto jsnack5

---
+++ jsnack5/script.js
@@ -0,0 +1,12 @@
+//Crea un array vuoto
+const numbersOdd = [];
+
+for (i= 0; i < 6; i++) {
+    //Chiedo all'utente di inserire 6 numeri
+    const number = Number(prompt("inserisci un numero"));
+    //Se il numero è dispari lo vado ad inserire nell'arrey
+    if ( number % 2 === 1) {
+        numbersOdd.push(number)
+    }
+}
+console.log(numbersOdd);
