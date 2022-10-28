7aedda40ad3a6a576fe87ddb1b4177b906937e3b
skiotto93 <fabio.forenza@hotmail.com>
2022-10-28 10:55:01 +0200

    tolta variabile inutile

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,10 +1,9 @@
 "use strict";
 //Creo un arrey con dei numeri interi
-const numbers = [15, 90, 23, 7, 37];
+const numbers = [15, 90, 23, 7, 37, 2, 1, 15, 84, 10];
 
 //Faccio la somma di tutti i numeri dispari contenuti nell'arrey
 let sum = 0;
-let oddnumbers = 0;
 
 for(let i = 0; i < numbers.length; i++) {
     if ( i % 2 === 0 ) {
